98089052484ee01fc8bd6bf8405b230cf27b9dd3
Rutenis Turcinas <rutenis.turcinas@gmail.com>
2020-03-28 21:23:19 +0200

    Project header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,20 +13,23 @@ import { environment } from '../environments/environment';
 import { ReferencesModule } from './references/references.module';
 import { VersionComponent } from './version/version.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     VersionComponent
   ],
-  imports: [
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ReferencesModule,
-  ],
+    imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+        AppRoutingModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        ReferencesModule,
+        MatToolbarModule,
+        MatButtonModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
