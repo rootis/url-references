c91fea2cdf02960f6b4e3de2cd5ec99aa23bc141
Rūtenis Turčinas <rutenis.turcinas@gmail.com>
2020-03-29 02:36:33 +0200

    Project header (#4)
    * Project header
    
    * Header was extracted to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,33 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
 import { ReferencesModule } from './references/references.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { VersionComponent } from './version/version.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VersionComponent
+    VersionComponent,
+    HeaderComponent
   ],
-  imports: [
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ReferencesModule,
-  ],
+    imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+        AppRoutingModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        ReferencesModule,
+        MatToolbarModule,
+        MatButtonModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
